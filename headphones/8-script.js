9cebb2b12a35b331f3a6e8cb62c1c9bd42b23d00
Mekdes Menji <mekdesmenji79@gmail.com>
2025-06-19 19:23:23 +0300

    all done

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -3,4 +3,10 @@ const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("show");
+
+  if (navMenu.classList.contains("show")) {
+    hamburger.textContent = "✕";
+  } else {
+    hamburger.textContent = "☰";
+  }
 });
